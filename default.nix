f698e8929792dbcf102ec7616bd24df4745b487d
Caspersonn <lucakasper8@gmail.com>
2025-09-18 11:24:35 +0200

    First push

---
+++ default.nix
@@ -0,0 +1,75 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+
+  inherit (pkgs) stdenv;
+
+  in
+  stdenv.mkDerivation rec {
+    name = "rapid7-insight-agent-${version}";
+    version = "4.0.18.46";
+
+    nativeBuildInputs = [ pkgs.dpkg pkgs.patchelf];
+
+    src = ./rapid7-insight-agent_4.0.18.46-1_amd64.deb;
+
+    unpackPhase = ''
+    dpkg-deb -x $src .
+    '';
+
+    buildInputs = [ pkgs.zlib ];
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out/bin
+      cp -r opt $out/
+
+      insight_agent="$out/opt/rapid7/ir_agent/components/insight_agent/${version}"
+      endpoint_broker="$out/opt/rapid7/ir_agent/components/endpoint_broker/1.8.2.0"
+      bootstrap="$out/opt/rapid7/ir_agent/components/bootstrap/2.12.0.1"
+      agent_core="$out/opt/rapid7/ir_agent/components/agent_core/1.2.2.2"
+
+      ir_agent="$insight_agent/ir_agent"
+
+      libs="$insight_agent/lib"
+
+      rpath="${pkgs.glibc}/lib:${pkgs.zlib.out}/lib:${pkgs.openssl.out}/lib:$libs"
+
+      echo ">> Patch main binary"
+
+      patchelf \
+        --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+        "$insight_agent"/{token_handler,connectivity_test,get_proxy}
+
+      patchelf \
+        --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+        --set-rpath "$rpath" \
+        "$ir_agent"
+
+      patchelf \
+        --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+        "$endpoint_broker"/rapid7_endpoint_broker
+
+      patchelf \
+        --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+        "$bootstrap"/{bootstrap,bootstrap_upgrader}
+
+      patchelf \
+        --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+        "$agent_core"/rapid7_agent_core
+
+      echo ">> Patch bundled .so files"
+      find "$libs" -type f -name '*.so*' -exec \
+      patchelf --set-rpath "$rpath" {} +
+
+      ln -s "$ir_agent" "$out/bin/ir_agent"
+
+      runHook postInstall
+    '';
+
+    meta = {
+      description = "Rapid7 Insight Agent (wrapped for NixOS)";
+      platforms = [ "x86_64-linux" ];
+      maintainers = [ "caspersonn" ];
+    };
+  }
